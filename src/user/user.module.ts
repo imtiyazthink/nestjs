2f3ac1c18acf9a134f3f4ed67a982dd8f7229e89
imtiyazthink <imtiyaz.mohammed@thinkitive.com>
2021-10-25 20:25:29 +0530

    bugfix : made sure that posts schema and posts module is imported in user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { UserSchema } from './schema/user.schema';
+import { User, UserSchema } from './schema/user.schema';
+import { Posts, PostsSchema } from 'src/posts/schema/posts.schema';
+import { PostsModule } from 'src/posts/posts.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+  imports: [
+    PostsModule,
+    MongooseModule.forFeature([
+    { name: User.name, schema: UserSchema },
+    { name: Posts.name, schema: PostsSchema }
+  ])], 
   controllers: [UserController],
   providers: [UserService]
 })
-export class UserModule {}
+export class UserModule { }
